8c4c00d2622324a532b7ce9c3508b75950b8da2a
Naufaall <naufallalala3103@gmail.com>
2025-11-20 08:48:12 +0800

    Mengganti Backgorund

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -38,7 +38,7 @@ const AboutUs = () => {
             The Purpose Behind Our Platform
           </h2>
 
-          <div className="bg-green-800 rounded-xl p-8 md:p-12 shadow-lg">
+          <div className="bg-neutral-900 rounded-xl p-6 md:p-10 shadow-xl">
             <div className="grid md:grid-cols-2 gap-8 items-center">
 
               {/* Text */}
@@ -61,7 +61,7 @@ const AboutUs = () => {
                 <div className="absolute top-4 right-4 bg-black bg-opacity-70 text-pink-300 px-4 py-2 rounded-lg font-bold text-xl">
                   100+
                 </div>
-                <div className="absolute bottom-4 left-4 bg-black bg-opacity-70 text-pink-300 px-4 py-2 rounded-lg font-bold text-xl">
+                <div className="absolute bottom-4 left-64 bg-black bg-opacity-70 text-pink-300 px-4 py-2 rounded-lg font-bold text-xl">
                   100+
                 </div>
               </div>
